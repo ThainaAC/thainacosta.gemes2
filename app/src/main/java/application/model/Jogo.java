192f23d4982432ccff93be9c92265a59c7d5141f
ThainaAC <alvesthaina568@gmail.com>
2023-11-08 17:56:23 +0000

    salvaf

--- app/src/main/java/application/model/Jogo.java
+++ app/src/main/java/application/model/Jogo.java
@@ -1,5 +1,6 @@
 package application.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -12,44 +13,12 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-@Entity 
-@Table(name="jogos")
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Entity
+@Table(name = "jogos")
 public class Jogo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public Genero getGenero() {
-        return genero;
-    }
-
-    public void setGenero(Genero genero) {
-        this.genero = genero;
-    }
-
-    public Set<Plataforma> getPlataformas() {
-        return plataformas;
-    }
-
-    public void setPlataformas(Set<Plataforma> plataformas) {
-        this.plataformas = plataformas;
-    }
-
     private String titulo;
 
     @ManyToOne
@@ -58,12 +27,25 @@ public class Jogo {
 
     @ManyToMany
     @JoinTable(
-        name = " jogos_possuem_plataformas",
-        joinColumns = @JoinColumn (name = "jogos-id"),
-        inverseJoinColumns = @JoinColumn  (name = "platformas-id")
+        name = "jogos_possuem_plataformas",
+        joinColumns = @JoinColumn(name = "jogos_id"),
+        inverseJoinColumns = @JoinColumn(name = "plataformas_id")
     )
+    private Set<Plataforma> plataformas = new HashSet<> ();
 
+    public int getId() {
+        return id;
+    }
 
-    private Set <Plataforma > plataformas  = new HashSet <>();
-    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public Set<Plataforma> getPlataformas() {
+        return plataformas;
+    }
 }
